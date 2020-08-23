089c9466e32ed5e01761a59f56da8557b5452bab
Mark Scherer <mark@Marks-MacBook-Pro.local>
2020-08-22 21:43:11 -0700

    controller test 1

--- server/frontend/src/components/LED_Strip_Controller.js
+++ server/frontend/src/components/LED_Strip_Controller.js
@@ -1,10 +1,59 @@
-import React, { Component } from 'react';
+import React, { Component } 					from 'react';
+import { Typography, Slider } 				from '@material-ui/core';
 
 class LEDStripController extends Component {
+	constructor(props) {
+    super(props)
+    this.state = {
+      colors : {
+      	red 	: 0,
+      	green : 0,
+      	blue 	: 0
+      }
+    }
+  }
+
+  updateColor(color, value) {
+  	console.log(`updating color: ${color}, ${value}`)
+
+  	let stateUpdates = {}
+  	stateUpdates[color] = value
+  	this.setState(stateUpdates)
+  }
+
   render() {
     return (
+      const {
+      	red,
+      	green,
+      	blue
+      } = this.state.colors
+
       <div className="LEDStripController">
-        Controller!
+      	<div className="colorControls"
+      		<div id="red-control">
+      			<Typography gutterBottom>
+	        		Red
+	      		</Typography>
+	      		<Slider value={red} onChange={(event, newValue) => { this.updateColor('red', newValue) }} aria-labelledby="continuous-slider" />
+      		</div>
+
+      		<div id="green-control">
+      			<Typography gutterBottom>
+	        		Green
+	      		</Typography>
+	      		<Slider value={green} onChange={(event, newValue) => { this.updateColor('green', newValue) }} aria-labelledby="continuous-slider" />
+      		</div>
+
+      		<div id="blue-control">
+      			<Typography gutterBottom>
+	        		Blue
+	      		</Typography>
+	      		<Slider value={blue} onChange={(event, newValue) => { this.updateColor('blue', newValue) }} aria-labelledby="continuous-slider" />
+      		</div>
+
+
+      	</div>
       </div>
     );
   }
