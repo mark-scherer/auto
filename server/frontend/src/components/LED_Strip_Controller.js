a3b0b26aae424c94fdfbbedf2e6f37aa32000d10
Mark Scherer <mark@Marks-MacBook-Pro.local>
2020-08-22 21:24:47 -0700

    frontent controller test 1

---
+++ server/frontend/src/components/LED_Strip_Controller.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LEDStripController extends Component {
+  render() {
+    return (
+      <div className="App">
+        Controller!
+      </div>
+    );
+  }
+}
+
+export default App;
