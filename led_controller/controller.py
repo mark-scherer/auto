e6025faa50b7091de88460af8d50666c1f38d68c
Mark Scherer <mark@Marks-MacBook-Pro.local>
2020-08-25 20:32:32 -0700

    gitignore, generalized pwm control

--- led_controller/controller.py
+++ led_controller/controller.py
@@ -5,26 +5,28 @@ LED_STRIP_GREEN_PIN = 12
 LED_STRIP_BLUE_PIN = 13
 
 class Controller:
-	def __init__(self, red_pin, green_pin, blue_pin):
+	'''
+		pin_guide is dict of (pin_name, pin_number) pairs
+	'''
+	def __init__(self, pin_guide):
 		GPIO.setwarnings(False)
 		GPIO.setmode(GPIO.BOARD)
-		GPIO.setup(red_pin, GPIO.OUT)
-		GPIO.setup(green_pin, GPIO.OUT)
-		GPIO.setup(blue_pin, GPIO.OUT)
 
-		self.red_pin = GPIO.PWM(red_pin, 1000)
-		self.green_pin = GPIO.PWM(green_pin, 1000)
-		self.blue_pin = GPIO.PWM(blue_pin, 1000)
+		self.pins = {}
+		for pin_name, pin_number value in pin_guide.items():
+			GPIO.setup(pin_number, GPIO.OUT)
+			self.pins[pin_name] = GPIO.PWM(pin_number, 1000)
+			self.pins[pin_name].start(0)
 
-		self.red_pin.start(0)	
-		self.green_pin.start(0)
-		self.blue_pin.start(0)
 
 	'''
 		update pwm intensity on specified pin (0-1)
 	'''
-	def set_pin(self, pin, value):
+	def set_pin(self, pin_name, value):
+		if pin_name not in self.pins:
+			raise ValueError('set_pin: invalid pin_name: {}'.format(pin_name))
+
 		if value < 0 or value > 1:
-			raise ValueError('value out of bounds: {}'.format(value))
+			raise ValueError('set_pin: value out of bounds: {}'.format(value))
 
 		pin.ChangeDutyCycle(value*100)
--- server/backend/index.py
+++ server/backend/index.py
@@ -7,82 +7,65 @@ sys.path.append('led_controller/')
 import controller as control
 
 port = 8080
-controller = control.Controller(control.LED_STRIP_RED_PIN, control.LED_STRIP_GREEN_PIN, control.LED_STRIP_BLUE_PIN)
+controller = control.Controller({
+    'red'       : control.LED_STRIP_RED_PIN,
+    'green'     : control.LED_STRIP_GREEN_PIN,
+    'blue'      : control.LED_STRIP_BLUE_PIN
+})
 frontend_path = os.path.join(os.getcwd(), 'server/frontend/build')
-print('frontend_path: {}'.format(frontend_path))
 
 class myHandler(SimpleHTTPRequestHandler):
     def __init__(self, request, client_address, server):
         super().__init__(request, client_address, server, directory=frontend_path)
 
-    def do_base(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/html')
-        self.end_headers()  
-
-        file = open(frontend_path, 'rb')
-        frontend_content = file.read()
-        file.close()
-        self.wfile.write(frontend_content)
-
-        print('sent frontend_content: {}'.format(frontend_content.decode('utf-8')))
+    def parse_query(query_string):
+        query = {}
+        for vp in query_string.split("&"):
+            split_vp = vp.split('=')
+            if len(split_vp) == 0:
+                raise ValueError('missing key for query param')
+            if len(split_vp) == 1:
+                raise ValueError('missing value for query param: {}'.format(split_vp[0]))
+            elif len(split_vp) == 2:
+                query[split_vp[0]] = split_vp[1]
+            else:
+                raise ValueError('malformed query param: {}'.format(split_vp[0]))
+        return query
 
     def do_update_led_strip(self, query_string):
-        try:
-            query = {}
-            for vp in query_string.split("&"):
-                split_vp = vp.split('=')
-                if len(split_vp) == 0:
-                    raise ValueError('missing key for query param')
-                if len(split_vp) == 1:
-                    raise ValueError('missing value for query param: {}'.format(split_vp[0]))
-                elif len(split_vp) == 2:
-                    query[split_vp[0]] = split_vp[1]
-                else:
-                    raise ValueError('malformed query param: {}'.format(split_vp[0]))
+        led_strip_pins = ['red', 'green', 'blue']
 
-            print('got color update query: {}'.format(query))
+        query = self.parse_query(query_string)
 
-            if 'red' not in query:
-                raise ValueError('missing red param')
-            if 'green' not in query:
-                raise ValueError('missing green param')
-            if 'blue' not in query:
-                raise ValueError('missing blue param')
+        for pin_name in led_strip_pins:
+            if pin_name not in query:
+                raise ValueError('missing param: {}'.format(pin_name))
 
+        for pin_name in led_strip_pins:
             try:
-                controller.set_pin(controller.red_pin, float(query['red']))
-            except Exception as ex:
-                raise ValueError('unsupported red intensity value: {}... {}'.format(query['red'], ex))
+                controller.set_pin(pin_name, float(query[pin_name]))
+            except Exception as error:
+                raise ValueError('unsupported {} intensity value: {}... {}'.format(pin_name, query[pin_name], error))
 
-            try:
-                controller.set_pin(controller.green_pin, float(query['green']))
-            except Exception as ex:
-                raise ValueError('unsupported green intensity value: {}... {}'.format(query['green'], ex))
-
-            try:
-                controller.set_pin(controller.blue_pin, float(query['blue']))
-            except Exception as ex:
-                raise ValueError('unsupported blue intensity value: {}... {}'.format(query['blue'], ex))
-
-            self.send_response(200)
-            self.end_headers() 
-
-        except Exception as ex:
-            self.send_error(400, message="error: {}".format(ex)) 
+        self.send_response(200)
+        self.end_headers()
+            
 
 
     #Handler for the GET requests
     def do_GET(self):
-        parsed = urlparse(self.path)
+        try:
+            parsed = urlparse(self.path)
 
-        if parsed.path == '/update_led_strip':
-            print('responding to request: {}'.format(self.path))
-            self.do_update_led_strip(parsed.query)
-        else:
-            # self.path = os.path.join(frontend_path, self.path)
-            print('responding to request w/ super.do_GET(): {}'.format(self.path))
-            super().do_GET()
+            if parsed.path == '/update_led_strip':
+                self.do_update_led_strip(parsed.query)
+            else:
+                # self.path = os.path.join(frontend_path, self.path)
+                print('responding to request w/ super.do_GET(): {}'.format(self.path))
+                super().do_GET()
+        except Exception as error:
+            print('excetion handling GET request ({}): {}'.format(self.path, error))
+            self.send_error(400, message="error: {}".format(error)) 
 
 
 server = HTTPServer(('', port), myHandler)
