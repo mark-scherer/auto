aa8ed700a6d9a70959f2c51cecffab9d1be09d2a
Mark Scherer <mark@Marks-MacBook-Pro.local>
2020-08-26 18:28:49 -0700

    reworked frontend 9

--- server/frontend/src/components/LEDStripControllers.js
+++ server/frontend/src/components/LEDStripControllers.js
@@ -21,10 +21,8 @@ class StripController extends Component {
       intensities
     } = this.state
 
-    console.log(`${this.label}: componentDidUpdate: ${JSON.stringify({ props: this.props, prevProps })}`)
     _.forEach(intensities, (value, channel) => {
       if (this.props.initialIntensities[channel] && this.props.initialIntensities[channel] !== prevProps.initialIntensities[channel]) {
-        console.log(`${this.label}: componentDidUpdate: updating channel: ${channel}: ${this.props.initialIntensities[channel]}`)
         this.updateIntensity(channel, this.props.initialIntensities[channel])
       }
     })
