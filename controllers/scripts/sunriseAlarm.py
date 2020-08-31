98c901f2e7379a9b839fc5e3f5a25ef90d015464
Mark Scherer <mark@Marks-MacBook-Pro.local>
2020-08-30 18:52:00 -0700

    sunrise test 5

--- controllers/scripts/sunriseAlarm.py
+++ controllers/scripts/sunriseAlarm.py
@@ -1,16 +1,16 @@
 from time import sleep
 
 # constants
-timestep 	= 100 		# ms
+timestep 	= 0.1 		# seconds
 
 '''
 	duration: 	seconds
 '''
 def sunriseAlarm(duration, pinController):
-	i_max = int(round(duration*1000 / timestep))
+	i_max = int(round(duration / timestep))
 	for i in range(i_max):
 		intensities = {
-			'white' 	: float(i) / float(i_max)
+			'white' 	: i / float(i_max)
 		}
 
 		print('sunriseAlarm ({}/{}): setting intensities: {}'.format(i, i_max, intensities))
