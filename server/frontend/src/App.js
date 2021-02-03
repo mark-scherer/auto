fd5b9e471258728891e49b6b15d4fe7f39a51be8
Mark Scherer <mark@Marks-MacBook-Pro.local>
2021-02-02 22:36:25 -0800

    added frontend update interval 2

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
     const full_url = `http://${CONFIG.server.host}:${CONFIG.server.port}/status`
     misc.makeRequest(full_url)
       .then(response => {
-        const serverState = JSON.parse(response.body)
+        const serverState = JSON.parse(response.body),
         console.log(`App: got serverState : ${JSON.stringify({ serverState })}`)
         this.setState({
           serverState
