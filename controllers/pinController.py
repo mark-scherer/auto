cd31ceac46b9797bd7c79571cddd6a6607e5d9d6
Mark Scherer <mark@Marks-MacBook-Pro.local>
2021-02-02 20:13:47 -0800

    updating apis: updated backend 1

--- controllers/pinController.py
+++ controllers/pinController.py
@@ -1,20 +1,35 @@
 import RPi.GPIO as GPIO
+import copy
+import json
 
+'''
+	class to handle actual interaction with device pins
+'''
 class PinController:
 	'''
-		pin_guide: dict of (pin_name, pin_number) pairs
+		pi: prep all channels & set to 0
 	'''
-	def __init__(self, pin_guide):
+	def piSetup(pin_guide):
 		GPIO.setwarnings(False)
 		GPIO.setmode(GPIO.BOARD)
+		for output, output_guide in pin_guide.items():
+			for channel, pin_number in output_guide.items():
+				GPIO.setup(pin_number, GPIO.OUT)
+				self.pin_objects[output][channel] = GPIO.PWM(pin_number, 1000)
+				self.pin_objects[output][channel].start(0)
 
-		self.pins = {}
-		self.current_values = {}
-		for pin_name, pin_number in pin_guide.items():
-			GPIO.setup(pin_number, GPIO.OUT)
-			self.pins[pin_name] = GPIO.PWM(pin_number, 1000)
-			self.pins[pin_name].start(0)
-			self.current_values[pin_name] = 0
+
+	'''
+		pin_guide: dict of format: {output_name_1, {channel_name_1: pin_number_1, ...,}, ...}
+	'''
+	def __init__(self, pin_guide):
+		self.pin_objects = copy.deepcopy(pin_guide)
+		self.current_values = copy.deepcopy(pin_guide)
+		for output, output_guide in pin_guide.items():
+			for channel, pin_number in output_guide.items():
+				self.pin_objects[output][channel] = None
+				self.current_values[output][channel] = 0
+		self.piSetup(pin_guide)
 
 
 	def getPinValues(self):
@@ -24,12 +39,14 @@ class PinController:
 	'''
 		update pwm intensity on specified pin (0-1)
 	'''
-	def setPin(self, pin_name, value):
-		if pin_name not in self.pins:
-			raise ValueError('setPin: invalid pin_name: {}'.format(pin_name))
+	def setPin(self, output, channel, value):
+		if output not in self.pin_objects:
+			raise ValueError(f'setPin: invalid output: {output}')
+		if channel not in self.pin_objects[output]:
+			raise ValueError(f'setPin: invalid channel for output: {json.dumps({'output': output, 'channel': channel})}')
 
 		if value < 0 or value > 1:
-			raise ValueError('setPin: value out of bounds: {}'.format(value))
+			raise ValueError(f'setPin: value out of bounds: {value}')
 
-		self.pins[pin_name].ChangeDutyCycle(value*100)
-		self.current_values[pin_name] = value
+		self.pin_objects[output][channel].ChangeDutyCycle(value*100)
+		self.current_values[output][channel] = value
--- server/backend/index.py
+++ server/backend/index.py
@@ -18,80 +18,61 @@ with open(os.path.join(_dir, '../configs/config_public.json')) as f:
 with open(os.path.join(_dir, '../configs/config_private.json')) as f:
     config_private = json.load(f)
 CONFIG = {**config_public, **config_private}
-frontend_path = os.path.join(_dir, '../frontend/build')
+FRONTEND_PATH = os.path.join(_dir, '../frontend/build')
 
 # server globals
-pinController   = pinControl.PinController({
-    **CONFIG['pins']['rgb'],
-    **CONFIG['pins']['white'],
-})
-scheduler       = schedule.Scheduler()
+
 
 class myHandler(SimpleHTTPRequestHandler):
-    def __init__(self, request, client_address, server):
-        super().__init__(request, client_address, server, directory=frontend_path)
+    self.pinController = pinControl.PinController(CONFIG['outputs'])
 
-    def sendResponse(self):
+    def __init__(self, request, client_address, server):
+        super().__init__(request, client_address, server, directory=FRONTEND_PATH)
+
+    def validateQuery(parsed_query, required_fields):
+        for field in required_fields:
+            if field not in parsed_query:
+                raise ValueError(f'query missing field: {field}')
+
+    def sendResponseStart(self):
         self.send_response(200)
         self.send_header("Access-Control-Allow-Origin", "*")
         self.end_headers()
 
-    def do_getCurrentValues(self):
-        response = json.dumps(pinController.getPinValues())
+    def do_control(parsed_query):
+        self.validateQuery(parsed_query, ['mode'])
 
-        self.sendResponse()
-        self.wfile.write(response.encode('utf-8'))
+        if parsed_query['mode'] == 'updateIntensity':
+            self.validateQuery(parsed_query, ['output', 'channel', 'value'])
+            self.pinController.setPin(**parsed_query)
+        else:
+            raise ValueError(f'unsupported mode: {parsed_query['mode']}')
+        self.sendResponseStart()         
 
-    def do_updatePins(self, query, stripPins):
-        
-        for pin_name in stripPins:
-            if pin_name not in query:
-                raise ValueError('missing param: {}'.format(pin_name))
-
-        for pin_name in stripPins:
-            try:
-                pinController.setPin(pin_name, float(query[pin_name][0]))
-            except Exception as error:
-                raise ValueError('unsupported {} intensity value: {}... {}'.format(pin_name, query[pin_name], error))
-
-        self.sendResponse()
-
-    def do_scheduleEvent(self, eventName, query, eventFunc, eventFuncArgs):
-        if 'datetime' not in query:
-            raise ValueError('missing param: {}'.format('datetime'))
-        
-        scheduler.addEvent(eventName, query['datetime'][0], eventFunc, eventFuncArgs)
-        print('scheduled {} for {}'.format(eventName, query['datetime']))
-
-        self.sendResponse()           
+    def do_status(parsed_query):
+        self.validateQuery(parsed_query, [])
+        response = {
+            'intensities': json.dumps(pinController.getPinValues())
+        }
+        self.sendResponseStart()
+        self.wfile.write(response.encode('utf-8'))  
 
     #Handler for the GET requests
     def do_GET(self):
         try:
-            parsed = urlparse(unquote(self.path))
-            query = parse_qs(parsed.query)
+            parsed_path = urlparse(unquote(self.path)).path[1:].split('/')
+            parsed_query = parse_qs(parsed.query)
 
-            # misc
-            if parsed.path == '/{}'.format(CONFIG['endpoints']['INITIAL_INTENSITIES_ENDPOINT']):
-                self.do_getCurrentValues()
+            if parsed_path[0] == 'control':
+                self.do_control(parsed_query)
+            elif parsed_path[0] == 'status':
+                self.do_status(parsed_query)
 
-            # direct controls
-            elif parsed.path == '/{}'.format(CONFIG['endpoints']['RGB_CONTROL_ENDPOINT']):
-                self.do_updatePins(query, CONFIG['pins']['rgb'].keys())
-            elif parsed.path == '/{}'.format(CONFIG['endpoints']['WHITE_CONTROL_ENDPOINT']):
-                self.do_updatePins(query, CONFIG['pins']['white'].keys())
-            
-            # event schedulers
-            elif parsed.path == '/{}'.format(CONFIG['endpoints']['SCHEDULE_SUNRISE_ENDPOINT']):
-                if 'duration' not in query:
-                    raise ValueError('missing param: {}'.format('duration'))
-                self.do_scheduleEvent('sunrise_alarm', query, sunrise.sunriseAlarm, [ float(query['duration'][0]), pinController ])
-
-            # default
+            # default: send frontend
             else:
                 super().do_GET()
         except Exception as error:
-            print('exception handling GET request ({}): {}'.format(self.path, error))
+            print('exception handling request ({}): {}'.format(self.path, error))
             self.send_error(400, message="error: {}".format(error)) 
 
 port = CONFIG['server']['port']
